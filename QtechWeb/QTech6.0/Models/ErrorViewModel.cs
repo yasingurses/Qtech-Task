e1597d077c4aaffa8bd34f712b81968e5e8ac288
Yasin Lütfi Gürses <term213@hotmail.com>
2022-12-25 12:03:42 +0300

    Final

--- QtechWeb/QTech6.0/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace QTech6._0.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
